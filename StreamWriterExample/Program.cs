da653542372430bfe86b8e6cfb06d203992d06db
parag <parag@DESKTOP-I2Q9QKQ>
2021-09-01 13:21:42 +0530

    Add project files.

---
+++ StreamWriterExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriterExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            FileStream f = new FileStream("c:\\output.txt", FileMode.Create);
+            StreamWriter s = new StreamWriter(f);
+
+            s.WriteLine("hello c#");
+            s.Close();
+            f.Close();
+            Console.WriteLine("File created successfully...");
+        }
+    }
+}
